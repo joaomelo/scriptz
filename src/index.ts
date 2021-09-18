3ff15a5aad1d271acd516625931a253529fcb63a
joão melo <j@melo.plus>
2021-09-18 14:25:16 -0300

    feat: small advances in importing external config file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import { join } from 'path';
+
+main();
+
+async function main(): Promise<void> {
+  const configFile = join(process.cwd(), 'sxz.config.js')
+  console.log({ configFile });  
+  const configExport = await import(configFile);
+  console.log({ configExport });  
+}
+
