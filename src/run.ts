2459b09984d068ca5770d342fb25e9ddf300da96
joão melo <j@melo.plus>
2021-10-04 19:27:15 -0300

    feat: draft for command run function

--- src/run.ts
+++ src/run.ts
@@ -5,5 +5,27 @@ export async function run(script: Script): Promise<void> {
 }
 
 async function once(command: Command, killer: CommandKiller): Promise<number> {
+  return new Promise(resolve => {
+    const tag = `${bgColor}${textColor}[${label}]${colors.reset} `;
+    console.info(`${tag}started`);
+
+    const scriptEnv = parseEnv({ file: envFile, vars: envVars });
+    const wrappedCommand = `${useNpx ? 'npx ' : ''}${command}`;
+
+    const runningProcess = spawn(wrappedCommand, {
+      shell: true,
+      env: scriptEnv
+    });
+
+    killer.kill = () => runningProcess.kill();
+
+    tagToConsole({ tag, stream: runningProcess.stdout });
+    tagToConsole({ tag, stream: runningProcess.stderr });
+
+    runningProcess.on('close', code => {
+      console.info(`${tag}exited with code ${code}`);
+      resolve(code);
+    });
+  });
 
 }
