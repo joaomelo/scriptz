3fd60fa147ef629a65d5a7bc9835c305599220e6
joão melo <j@melo.plus>
2021-10-19 20:56:05 -0300

    chore: renamed lab to migrate

---
+++ migrate/index.ts
@@ -0,0 +1,17 @@
+import { sxz, TextColor, BgColor, Command } from '../src/types';
+
+const firebaseEmulators: Command = {
+  name: 'emulators',
+  instruction: 'firebase emulators:start',
+  textColor: TextColor.White,
+  bgColor: BgColor.Yellow
+};
+
+const echo: Command = {
+  name: 'echo',
+  instruction: 'echo "sxz"',
+  textColor: TextColor.Black,
+  bgColor: BgColor.White
+};
+
+sxz([ firebaseEmulators, echo ]);
