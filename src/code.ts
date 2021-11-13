3799263c741f4d7e415f66b302defc8c0be374ae
joão melo <j@melo.plus>
2021-11-13 18:31:46 -0300

    refactor: removed business logic from code

--- src/code.ts
+++ src/code.ts
@@ -1,8 +1,6 @@
-type Code = number | number[] | undefined | null;
+type Code = number | undefined | null;
 
 export function solveCode(code: Code) {
   if (code === null || code === undefined) return 1;
-  if (typeof code === "number") return code;
-
-  return code.every((c) => c === 0) ? 0 : 1;
+  return code;
 }
