6be37c1d2c8aebeda8290be603f7e8e9488ec08e
joão melo <j@melo.plus>
2021-11-26 19:35:38 -0300

    fix: [skip ci] changed config file name to avoid conflit with bin

--- src/script.js
+++ src/script.js
@@ -3,15 +3,13 @@ import { pathToFileURL } from "url";
 
 export async function extractUserScripts() {
   const configFilePath = resolveFilePath();
-  console.log("test1");
   const exportedScripts = await import(configFilePath);
-  console.log("test2");
   const scripts = Object.values(exportedScripts);
   return scripts;
 }
 
 function resolveFilePath() {
-  const fileName = "sqript.js";
+  const fileName = "sqript.config.js";
   const fileAbsolutePath = resolve(process.cwd(), fileName);
   const fileUrl = pathToFileURL(fileAbsolutePath);
   return fileUrl.pathname;
